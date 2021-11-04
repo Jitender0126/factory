823f01086d6012266cb850f8e166471e92a05272
Jitender khokhar <Jitender0126@gmail.com>
2021-11-04 22:46:14 +0000

    added the seeder class and DB seeder

--- database/seeders/CarTableSeeder.php
+++ database/seeders/CarTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Car;
 use Illuminate\Database\Seeder;
 
 class CarTableSeeder extends Seeder
@@ -13,6 +13,22 @@ class CarTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $bmw=new Car;
+        $bmw->make="BMW";
+        $bmw->model="E43i";
+        $bmw->year=2008;
+        $bmw->mileage=12000;
+        $bmw->displacement=3.2;
+        $bmw->save();
+
+
+        $vw=new Car;
+        $vw->make="VW";
+        $vw->model="Polo";
+        $vw->year=2012;
+        $vw->mileage=20000;
+        $vw->displacement=2.2;
+        $vw->save();
+
     }
 }
