a99f2e3194713f6fa70fcaa95e167bd161c069fe
Jitender khokhar <Jitender0126@gmail.com>
2021-11-05 00:08:16 +0000

    added 1-2-1 relationship with car & car-owner

---
+++ database/migrations/2021_11_04_235236_create_car_owners_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarOwnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_owners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->bigInteger('car_id')->unsigned();
+            $table->timestamps();
+
+
+            $table->foreign('car_id')->references('id')->on('cars')
+            ->onDelete('cascade')->onUpdate('cascade');
+            ;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_owners');
+    }
+}
